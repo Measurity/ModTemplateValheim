87b31ea42e15721910923b86ebb20c8712deb6d2
Measurity <measuring.infinity@gmail.com>
2021-02-13 03:20:12 +0100

    Fixed .org to .com

--- MethodCallCommand/Mod.cs
+++ MethodCallCommand/Mod.cs
@@ -2,7 +2,7 @@
 
 namespace MethodCallCommand
 {
-    [BepInPlugin("org.github.measurity.methodcallcommandplugin", "MethodCallCommand", "1.0.0.0")]
+    [BepInPlugin("com.github.measurity.methodcallcommandplugin", "MethodCallCommand", "1.0.0.0")]
     public class Mod : BaseUnityPlugin
     {
         private void Awake()
